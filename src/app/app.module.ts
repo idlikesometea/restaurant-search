3252ce51cf50c7cabdda70270d3e42ce53dfd30b
idlikesometea <im@virket.com>
2020-07-13 20:34:04 -0500

    add basic components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SearchModule } from './search/search.module';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SearchModule,
     NgbModule
   ],
   providers: [],
