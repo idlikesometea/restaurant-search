0c8c76c2292a8eb6fc5e549e094770540c970141
idlikesometea <im@virket.com>
2020-07-13 21:48:12 -0500

    get event from input and debounce search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
+import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
+  searchRadius: number = 0;
+  radiusOptions = [0, 10, 50, 100];
+  query: string = '';
+  keyeventSub: Subscription;
+  @ViewChild('searchbar', { static: true }) input: ElementRef;
+
 
   constructor() { }
 
   ngOnInit(): void {
+    this.keyeventSub = fromEvent<any>(this.input.nativeElement, 'keyup')
+      .pipe(
+        map((input: any) => input.currentTarget.value),
+        debounceTime(350),
+        distinctUntilChanged()
+      )
+      .subscribe(query => {
+        this.search(query);
+      })
+  }
+
+  search(query: string,) {
+    console.log(query, this.searchRadius, 'doSearch');
+  }
+
+  ngOnDestroy() {
+    this.keyeventSub.unsubscribe();
   }
 
 }
