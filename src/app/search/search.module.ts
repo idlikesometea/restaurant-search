7675f8432798a7c98d91892794228db007ed84c4
idlikesometea <im@virket.com>
2020-07-14 20:38:07 -0500

    Revert "add ngrx"
    This reverts commit 0983cf6acb16cf72f6f05351b9aa8f3390e200de.

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search.component';
 import { FormsModule } from '@angular/forms';
-import { StoreModule } from '@ngrx/store';
 
 
 
@@ -10,8 +9,7 @@ import { StoreModule } from '@ngrx/store';
   declarations: [SearchComponent],
   imports: [
     CommonModule,
-    FormsModule,
-    StoreModule.forRoot({}, {})
+    FormsModule
   ]
 })
 export class SearchModule { }
