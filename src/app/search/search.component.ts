7abdc2edb42e9d894ccc5f6708dd425cdcb024ec
idlikesometea <im@virket.com>
2020-07-15 22:22:04 -0500

    manage results in service

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
-import { fromEvent, Subscription, Observable } from 'rxjs';
-import { map, debounceTime, distinctUntilChanged, filter } from 'rxjs/operators';
+import { Subscription, Observable } from 'rxjs';
 import { SearchStore } from 'src/store/state/search.state';
 import { ISearchResponse } from 'src/models/search.model';
-
+import { SearchService } from './search.service';
 
 @Component({
   selector: 'app-search',
@@ -14,52 +13,41 @@ export class SearchComponent implements OnInit, OnDestroy {
   searchRadius: number = 5;
   radiusOptions = [5, 10, 25, 50];
   query: string = '';
-  keyeventSub: Subscription;
-  @ViewChild('searchbar', { static: true }) input: ElementRef;
+  searchEvent$: Subscription;
+  @ViewChild('searchbar', { static: true }) searchbar: ElementRef;
   searchResults$: Observable<ISearchResponse[]>;
   isLoading$: Observable<boolean>;
   error$: Observable<string>;
-  resultsLength$: Observable<number>;
   showAlert: boolean = false;
-  filtered = [];
+  results;
 
   constructor(
-    private store: SearchStore
-  ) { }
+    private store: SearchStore,
+    private searchService: SearchService,
+  ) {
+    this.searchService.getResults$().subscribe(results => {
+      this.results = results;
+    });
+  }
 
   ngOnInit(): void {
-    this.keyeventSub = fromEvent<any>(this.input.nativeElement, 'keyup')
-      .pipe(
-        map(input => input.currentTarget.value.trim()),
-        debounceTime(350),
-        distinctUntilChanged()
-      )
+    this.searchEvent$ = this.searchService.keyupEvent(this.searchbar.nativeElement)
       .subscribe(query => {
         this.search(query);
       });
 
     this.searchResults$ = this.store.searchResults$();
-    this.searchResults$
-      .subscribe(results =>
-        this.filtered = results.map(result => ({...result, checked: false}))
-      );
-
-    this.resultsLength$ = this.store.resultsLength$();
-    this.resultsLength$.subscribe();
+    this.searchResults$.subscribe(results => this.searchService.storeResults(results));
 
     this.isLoading$ = this.store.isLoading$();
     this.isLoading$.subscribe();
 
     this.error$ = this.store.searchError$();
     this.error$.subscribe(error => {
-      if (error) {
-        this.showAlert = true;
-        setTimeout(()=> {
-          this.showAlert = false;
-        }, 4000);
-      } else {
+      this.showAlert = (error !== '') || false;
+      setTimeout(() => {
         this.showAlert = false;
-      }
+      }, 4000);
     });
   }
 
@@ -71,13 +59,21 @@ export class SearchComponent implements OnInit, OnDestroy {
     this.store.search(query, this.searchRadius);
   }
 
+  filter(query: string) {
+    this.searchService.filter(query);
+  }
+
+  sort(key) {
+    this.searchService.sort(key);
+  }
+
   onItemClicked(restaurant) {
-    const index = this.filtered.findIndex(i => i.id === restaurant.id);
-    this.filtered[index].checked = !restaurant.checked;
+    const index = this.results.findIndex(i => i.id === restaurant.id);
+    this.results[index].checked = !restaurant.checked;
   }
 
   ngOnDestroy() {
-    this.keyeventSub.unsubscribe();
+    this.searchEvent$.unsubscribe();
   }
 
 }
--- src/app/search/search.service.ts
+++ src/app/search/search.service.ts
@@ -1,20 +1,47 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ElementRef } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
-import { ISearchResponse } from '../../models/search.model';
+import { Observable, fromEvent, Subject } from 'rxjs';
+import { ISearchResponse, ISearch } from '../../models/search.model';
+import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
   API_URL = 'https://5f0e71a9704cdf0016eaf02e.mockapi.io/api/v1/';
+  results$ = new Subject();
   constructor(
     private http: HttpClient
   ) { }
 
-  search(query): Observable<any> {
+  search(query: ISearch): Observable<any> {
     return this.http.get<ISearchResponse[]>(this.API_URL + 'restaurants');
+  }
+
+  filter(query: string) {
+    console.log('filter', query);
+  }
+
+  sort(key) {
+    console.log('sort', key);
+  }
+
+  getResults$() {
+    return this.results$.asObservable();
+  }
+
+  storeResults(results) {
+    const storedResults = results.map(result => ({...result, checked: false}));
+    this.results$.next([...storedResults]);
+  }
+
+  keyupEvent(element): Observable<string>{
+    return fromEvent<any>(element, 'keyup')
+      .pipe(
+        map(input => input.currentTarget.value.trim()),
+        debounceTime(350),
+        distinctUntilChanged()
+      )
   }
 
 }
